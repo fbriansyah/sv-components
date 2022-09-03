66143db8196ba6413baaab202be0264444746136
Febriansyah <febrian@efner.id>
2022-09-03 13:09:57 +0700

    first init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import 'remixicon/fonts/remixicon.css';
+</script>
+
+<main>
+  <h1>Hello World</h1>
+  <i class="ri-home-8-line"></i>
+</main>
+
+<style lang="postcss">
+  main {
+    @apply p-2;
+  }
+
+  h1 {
+    @apply text-xl font-semibold text-gray-700;
+  }
+  i {
+    @apply text-red-700 text-xl;
+  }
+</style>
